3182984c7c3786cc23fd0380af71a2c3916c162b
Alejandro Castillo <alecas.1297@gmail.com>
2019-09-08 13:07:13 -0400

    first commit

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,6 +2,16 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for (int i = 0; i < 39; i++) {
+            System.out.println(a + "   ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.*;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -10,9 +12,40 @@ public class DuplicateWord {
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
-
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        List<String> list = Arrays.asList(st.split(" "));
 
+        Set<String> uniqueWords = new HashSet<String>(list);
+        for (String word : uniqueWords) {
+            System.out.println(word + ": " + Collections.frequency(list, word));
+
+
+        }
+        System.out.println(getWordLengths(st));
     }
+    public static ArrayList<Integer> getWordLengths( String str1 )
+    {
 
+        String str = " " + str1 + " ";
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for ( int i = 0; i < str.length(); i++ )
+        {
+            if ( str.charAt(i) == ' ' )
+            {
+                for ( int j = i+1; j < str.length() ; j++)
+                {
+                    if ( str.charAt(j) == ' ')
+                    {
+                        list.add( j - i - 1 );
+                        j = str.length();
+                    }
+                }
+            }
+        }
+        return list;
+    }
 }
+
+
